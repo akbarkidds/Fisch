2abc7ceb719cfc522b94bfbbfe495bfc9160f869
akbarkidds <55218323+akbarkidds@users.noreply.github.com>
2025-02-08 03:07:11 +0800

    Update saveManager.lua

--- saveManager.lua
+++ saveManager.lua
@@ -1,7 +1,7 @@
 local httpService = game:GetService("HttpService")
 
 local SaveManager = {} do
-	SaveManager.Folder = "KiddsSettings"
+	SaveManager.Folder = "Kidds"
 	SaveManager.Ignore = {}
 	SaveManager.Parser = {
 		Toggle = {
